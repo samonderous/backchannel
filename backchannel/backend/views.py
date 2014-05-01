b1c83553526cc43ffaedf019a8f688b5da425b3f
saureen <ubuntu@domU-12-31-39-09-D5-C8.compute-1.internal>
2014-05-01 03:21:24 +0000

    email share / invite and some org name / domain stuff

--- backchannel/backend/views.py
+++ backchannel/backend/views.py
@@ -30,16 +30,19 @@ except:
 @csrf_exempt
 def auth(request):
     response = {'status': 1, 'domain': ''}
-    email = request.POST.get('email')
+    email = request.POST.get('email', '')
     udid = request.POST.get('udid')
-    domain = ''
+
+    if re.match('^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$', email) is None:
+        return HttpResponse(simplejson.dumps(response), content_type="application/json")
+
+    domain = email.split('@')[1]
     try:
-        domain = email.split('@')[1]
         org = Org.objects.get(domain=domain)
         # TODO: Make async
         send_email.send_verify_email(org, email, udid)
     except Exception, e:
-        print "%s" % e
+        response['status'] = 2
         return HttpResponse(simplejson.dumps(response), content_type="application/json")
 
     emp_count = User.objects.filter(org=org).count()
@@ -66,7 +69,7 @@ def verify(request):
         org = Org.objects.get(domain=domain)
         user.org = org
         user.save()
-        response = {'status': 0}
+        response = {'status': 0, 'name': org.name, 'domain': org.domain}
         return HttpResponse(simplejson.dumps(response), content_type="application/json")
 
     return render_to_response('verify.html', {'u' : request.GET.get('u') })
@@ -175,7 +178,7 @@ def stream(request):
     response = {'status': 0, 'secrets': secrets_list}
     return HttpResponse(simplejson.dumps(response), content_type="application/json")
 
-def getlatestsecrets(request):
+def getlatestposts(request):
 
     response = {'status': 1}
     udid = request.GET.get('udid')
@@ -212,11 +215,48 @@ def getlatestsecrets(request):
     response = {'status': 0, 'secrets': secrets_list}
     return HttpResponse(simplejson.dumps(response), content_type="application/json")
 
+def getolderposts(request):
+
+    response = {'status': 1}
+    udid = request.GET.get('udid')
+    slid = request.GET.get('lsid')
+
+    try:
+        user = User.objects.get(udid=udid)
+    except Exception, e:
+        return HttpResponse(simplejson.dumps(response), content_type="application/json")
+
+    # TODO: Fix this up if traffic ever warrants [:50] will be an issue
+    secrets = Secret.objects.filter(org=user.org, id__lt=slid).order_by('id')[:10]
+
+    secrets_list = []
+    for s in secrets:
+        try:
+            us = UserSecret.objects.get(secret=s, user=user)
+            vote = us.vote
+        except Exception, e:
+            vote = UserSecret.VOTE_NONE
+
+        time_ago = int(time.time()) - s.time_created
+        secret_dict = {
+            'sid': s.id,
+            'secrettext': s.secrettext,
+            'time_created': s.time_created,
+            'time_ago': _time_str(time_ago),
+            'agrees': s.agrees,
+            'disagrees': s.disagrees,
+            'vote': vote
+        }
+        secrets_list.append(secret_dict)
+
+    response = {'status': 0, 'secrets': secrets_list}
+    return HttpResponse(simplejson.dumps(response), content_type="application/json")
+
+
 def resendemail(request):
     email = request.GET.get('email')
-    udid = request.POST.get('udid')
+    udid = request.GET.get('udid')
 
-    print "Need to resend email to %s" % email
     try:
         domain = email.split('@')[1]
         org = Org.objects.get(domain=domain)
@@ -229,3 +269,13 @@ def resendemail(request):
     return HttpResponse(simplejson.dumps(response), content_type="application/json")
 
 
+def signup(request):
+    ic = request.GET.get('c')
+    try:
+        tc = TrackClick.objects.get(invite_code=ic)
+        tc.clicked = 1
+        tc.save()
+    except Exception, e:
+        pass
+
+    return redirect('http://signup.backchannel.it')
