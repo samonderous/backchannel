ccdaf2d8807152eb500d1241f8b56a2537b2e24a
Saureen Shah <saureen@Saureens-MacBook-Pro.local>
2014-09-11 17:42:41 -0700

    checkpoint at backend and some UI code

--- BackChannel/BCAPIClient.h
+++ BackChannel/BCAPIClient.h
@@ -29,5 +29,7 @@ typedef  void (^FailureCallback)(AFHTTPRequestOperation *operation, NSError *err
 - (void)sendVerification:(SuccessCallback)success failure:(FailureCallback)failure;
 - (void)getLatestPosts:(void (^)(NSMutableArray*))success failure:(FailureCallback)failure withTopSid:(int)topSid;
 - (void)getOlderPosts:(void (^)(NSMutableArray*))success failure:(FailureCallback)failure withLastSid:(int)lastSid;
+- (void)fetchCommentsFor:(BCSecretModel*)model success:(void (^)(NSMutableArray*))success failure:(FailureCallback)failure;
+- (void)createComment:(NSString*)text onSecret:(BCSecretModel*)model success:(SuccessCallback)success failure:(FailureCallback)failure;
 
 @end
