4f7148db520d310636db3a209512a5d088d1f390
Saureen Shah <saureen@Saureens-MacBook-Pro.local>
2014-09-12 02:09:39 -0700

    remove debug

--- BackChannel/BCCommentsViewController.m
+++ BackChannel/BCCommentsViewController.m
@@ -508,7 +508,7 @@ static const CGFloat kCommentPadding = 30.0;
             cell.avatar.image = [UIImage imageNamed:@"avatar_general.png"];
         }
         cell.avatar.clipsToBounds = YES;
-        [cell.avatar debug];
+        //[cell.avatar debug];
         NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
         style.minimumLineHeight = 22.0;
         style.maximumLineHeight = 22.0;
